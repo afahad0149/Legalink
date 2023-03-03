3d93c505956a539e8f14938a7d7ed7e750bb7a15
afahad0149 <fahad.akibahmed@gmail.com>
2023-03-04 05:39:03 +0600

    refactored login form

--- client/src/app/pages/login-page/login-page.component.ts
+++ client/src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  hide = true;
+  errorMessage = '';
 
+  loginForm = this.fb.group({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  constructor(
+    private auth: AuthService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  handleSubmit() {
+    const { email, password } = this.loginForm.value;
+    if (email && password) {
+      this.auth.login(email, password).subscribe({
+        next: (res) => {
+          const user = res.body;
+          this.router.navigate(['home']);
+        },
+        error: (err) => {
+          this.errorMessage = err.error;
+          setTimeout(() => {
+            this.errorMessage = '';
+          }, 3000);
+        },
+      });
+    }
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
