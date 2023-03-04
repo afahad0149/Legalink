002022885d1cf02cfd6b41eae79d99749a5d1241
afahad0149 <fahad.akibahmed@gmail.com>
2023-03-04 20:22:37 +0600

    search component UI elements added

--- client/src/app/pages/client-search-page/client-search-page.component.ts
+++ client/src/app/pages/client-search-page/client-search-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-client-search-page',
   templateUrl: './client-search-page.component.html',
-  styleUrls: ['./client-search-page.component.scss']
+  styleUrls: ['./client-search-page.component.scss'],
 })
 export class ClientSearchPageComponent {
-
+  searchForm = this.fb.group({
+    lawyerServiceCategory: new FormControl('', [Validators.required]),
+    lawyerConsultationFee: new FormControl(500),
+  });
+  constructor(private fb: FormBuilder) {}
 }
