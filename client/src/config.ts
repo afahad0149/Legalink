7367843baa0334ada143b058a9f26a8ba9ee0427
afahad0149 <fahad.akibahmed@gmail.com>
2023-11-17 14:36:34 +0600

    updated backend live url

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-export const baseBackendURL = 'https://legalink.fly.dev';
+export const baseBackendURL = 'https://legalink.onrender.com';
 // export const baseBackendURL = 'http://localhost:3000';
 export const baseFrontendURL = 'https://legalink.vercel.app';
 // export const baseFrontendURL = 'http://localhost:4200'
